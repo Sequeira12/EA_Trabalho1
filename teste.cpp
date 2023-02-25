6ba1b0218e2c5d6405fdec895136b32ee5a7eb7a
davidsilva02 <davidsilva200210.ds@gmail.com>
2023-02-25 18:42:44 +0000

    alteracoes no meu codigo, infelizmente, não funciona

--- teste.cpp
+++ teste.cpp
@@ -117,36 +117,36 @@ void imprimeQRCode(int tam){
 	cout << "+\n";
 }
 
-bool contarPretosLinha(int l){
+int contarPretosLinha(int l){
 	int count=0;
 	for (int c=0;c<tamanho;c++){
 		if (mat[l][c]==1) count+=1;
 	}
-	return count<=lb[l];
+	return count;
 }
 
-bool contarPretosColuna(int c){
+int contarPretosColuna(int c){
 	int count=0;
 	for (int l=0;l<tamanho;l++){
 		if (mat[l][c]==1) count+=1;
 	}
-	return count<=cb[c];
+	return count;
 }
 
-bool contarPretosTransicaoLinha(int l){
+int contarPretosTransicaoLinha(int l){
 	int count=0;
 	for (int c=0;c<tamanho-1;c++){
 		if ((mat[l][c]==0 && mat[l][c+1]==1) ||(mat[l][c+1]==0 && mat[l][c]==1)) count++;
 	}
-	return count<=lt[l];
+	return count;
 }
 
-bool contarPretosTransicaoColuna(int c){
+int contarPretosTransicaoColuna(int c){
 	int count=0;
 	for (int l=0;l<tamanho-1;l++){
 		if ((mat[l][c]==0 && mat[l+1][c]==1) ||(mat[l+1][c]==0 && mat[l][c]==1)) count++;
 	}
-	return count<=ct[c];
+	return count;
 }
 
 bool contarPretosQuadrante(int l, int c){
@@ -189,7 +189,7 @@ bool contarPretosDiagonal(int l, int c){
 			if (mat[i][i]==1) count++;
 			if(mat[tamanho-i-1][i]==1) count_++;
 		}
-		return count<=db[0] && count_<db[1];
+		return (count<=db[0] && count_<=db[1]);
 	}
 	//pertence a diagonal principal
 	else if (l==c){
@@ -211,27 +211,42 @@ bool validarElemento(int l, int c){
 	bool flag;
     // se todas as condições se verificarem o elemento é valido para ficar preto
 	mat[l][c]=1;
-    flag = contarPretosLinha(l) && contarPretosTransicaoLinha(l) && contarPretosColuna(c) && contarPretosTransicaoColuna(c) && contarPretosQuadrante(l,c) && contarPretosDiagonal(l,c);
+    flag = contarPretosLinha(l)<=lb[l] && contarPretosTransicaoLinha(l)<=lt[l] && contarPretosColuna(c)<=cb[c] && contarPretosTransicaoColuna(c)<=ct[c]
+	&& contarPretosQuadrante(l,c) && contarPretosDiagonal(l,c);
 	mat[l][c]=0;
 	return flag;
 
+}
+
+bool verificacaoFinal()
+{
+	int teste;
+	for (int i=0;i<tamanho;i++){
+		if((teste=contarPretosLinha(i))!=lb[i]) return false;
+		if((teste=contarPretosColuna(i))!=cb[i]) return false;
+		if((teste=contarPretosTransicaoColuna(i))!=ct[i]) return false;
+		if(teste=(contarPretosTransicaoLinha(i)!=lt[i])) return false;
+	}
+	return true;
 }
 
 
 void procurar_qr_codes(int l,int c, int size_max){
 	if (c==size_max-1 && l==size_max-1){
 		if(mat[l][c]==0 && validarElemento(l,c)){
-			cout << "encontrou" << "\n";
+			//cout << "encontrou" << "\n";
 			mat[l][c]=1;
 			solutions.push_back(mat);
+			mat[l][c]=0;
 			return;
 		}
 		//solutions.push_back(mat);
 		return;
 	}
+	
     for (int i=0;i<size_max;i++){
         for (int j=0;j<size_max;j++){
-			if (mat[i][j]==0 && validarElemento(i,j) && i!=l && j!=c){
+			if (mat[i][j]==0 && validarElemento(i,j)){
 				mat[i][j]=1;
 
 				procurar_qr_codes(i,j+1,size_max);
@@ -245,7 +260,6 @@ void procurar_qr_codes(int l,int c, int size_max){
 
 int main()
 {
-
 	int numeroQR;
 	cin >> numeroQR;
 
@@ -261,28 +275,29 @@ int main()
 
         mat=vector <vector<int>> (tamanho,(tamanho,vector<int>(tamanho,0)));
 		
+		
+
+		
         LeituraQrCode(numero);
 
         procurar_qr_codes(0,0,tamanho);
 
-		if(solutions.size()>=1){
-
-		mat=solutions[0];
-		imprimeQRCode(tamanho);
+		if(solutions.size()==1){
+			mat=solutions[0];
+			imprimeQRCode(tamanho);
 		}
-
-		// if (Same == 1)
-		// {
-		// 	imprimeQRCode(tamanho);
-		// }
-		// else if (Same > 1)
-		// {
-		// 	printf("INVALID: %d QR Codes generated!\n", Same);
-		// }
-		// else
-		// {
-		// 	printf("DEFECT: No QR code generated!\n");
-		// }
+		else if (solutions.size()>1){
+			for (int i=0;i<solutions.size();i++){
+				mat=solutions[i];
+				imprimeQRCode(tamanho);
+				cout << endl<<endl;
+			}
+			printf("INVALID: %ld QR Codes generated!\n", solutions.size());
+		}
+		else{
+			printf("DEFECT: No QR code generated!\n");
+		}
+	
 	}
 
 	return 0;
